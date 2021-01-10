79e111fc5e947559c0a3dd3bea11bcbce6183b0a
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-10 21:47:58 +0100

    about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 const About = () => {
-  return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
+    return (
+        <section className="section about-section">
+            <h1 className="section-title">about us</h1>
+            <p>
+            Jelly sweet roll jelly beans biscuit pie macaroon chocolate donut. Carrot cake caramels pie sweet apple pie tiramisu carrot cake. Marzipan marshmallow croissant tootsie roll lollipop. Cupcake lemon drops bear claw gummies. Jelly bear claw gummi bears lollipop cotton candy gummi bears chocolate bar cake cookie. Cupcake muffin danish muffin cookie gummies. Jelly beans tiramisu pudding. Toffee soufflé chocolate cake pastry brownie. Oat cake halvah sweet roll cotton candy croissant lollipop. Macaroon tiramisu chocolate bar candy candy carrot cake jelly sweet. Gummies croissant macaroon dessert. Chocolate cake dragée pie.
+            </p>
+        </section>
+    )
 }
 
 export default About
