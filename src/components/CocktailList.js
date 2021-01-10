3de206ea34e76d5db83f60c30c378779b08afcc4
Meruyert Karim <meruyert.karim@nu.edu.kz>
2021-01-10 22:18:51 +0100

    Fetching Data and Search form

--- src/components/CocktailList.js
+++ src/components/CocktailList.js
@@ -4,11 +4,25 @@ import Loading from './Loading'
 import { useGlobalContext } from '../context'
 
 const CocktailList = () => {
-  return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
-  )
+    const {cocktails, loading} = useGlobalContext();
+
+    if (loading) {
+        return <Loading/>
+    }
+
+    if(cocktails.length < 1){
+        return (
+            <h2 className="section-title">
+                no cocktails matched your search criteria
+            </h2>
+        )
+    }
+
+    return (
+        <div>
+            <h2>cocktail list component</h2>
+        </div>
+    )
 }
 
 export default CocktailList
--- src/context.js
+++ src/context.js
@@ -5,11 +5,45 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+    const [loading, setLoading] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('a');
+    const [cocktails, setCocktails] = useState([]);
+
+    const fetchDrinks = async () => {
+        setLoading(true);
+        try {
+            const response = await fetch(`${url}${searchTerm}`)
+            const data = await response.json();
+            const {drinks} = data;
+            if (drinks) {
+                
+            }
+            else {
+                setCocktails([])
+            }
+            setLoading(false)
+
+        } catch(error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchDrinks();
+    }, [searchTerm])
+
+    return (
+        <AppContext.Provider 
+            value={{loading, cocktails, setSearchTerm}}
+        >
+            {children}
+        </AppContext.Provider>
+    )
 }
 // make sure use
 export const useGlobalContext = () => {
-  return useContext(AppContext)
+    return useContext(AppContext)
 }
 
 export { AppContext, AppProvider }
